adc87fee39225387367b918a62dc04ab96f0da0a
sonuyb <125265362+sonuyb@users.noreply.github.com>
2024-02-16 11:30:17 +0000

    Update program8.py

--- dictonaries/program8.py
+++ dictonaries/program8.py
@@ -1,7 +1,11 @@
-letter_counts = {}
+def count_letters(input_string):
+    letter_counts = {}
+    for char in input_string:
+        if char.isalpha():
+            char = char.lower()
+            letter_counts[char] = letter_counts.get(char, 0) + 1
+    return letter_counts
 input_string = input("Enter a string: ")
-for char in input_string:
-    if char.isalpha():
-        char = char.lower()
-        letter_counts[char] = letter_counts.get(char, 0) + 1
-print("Dictionary with letter counts:", letter_counts)
+result = count_letters(input_string)
+print("Dictionary with letter counts:", result)
+
